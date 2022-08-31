8e970da4992d5e14de7021a57e3c8fdc42fac65e
Marek Nagy <62247071@cuni.cz>
2022-08-31 12:34:21 +0200

    Added server for fast uploads from phone{other PCs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from argparse import ArgumentParser, Namespace
 from moviepy.editor import VideoFileClip, ImageClip
 import numpy as np
@@ -18,58 +19,137 @@ def get_args() -> Namespace:
 
     return args
 
-def avg_start(example_img: np.ndarray):
-    return np.zeros(example_img.shape, dtype=np.uint64)
+class BaseProcessing:
+    __slots__ = ('res_photo')
 
-def avg_step(cumulative_img: np.ndarray, new_img: np.ndarray):
-    return cumulative_img + new_img
+    def __init__(self) -> None:
+        self.res_photo = None
 
-def avg_final(cumulative_img: np.ndarray, amount: int):
-    return np.round( cumulative_img / amount )
+    @abstractmethod
+    def start(self, example_img: np.ndarray) -> None:
+        raise NotImplementedError()
 
-def max_start(example_img: np.ndarray):
-    return np.zeros(example_img.shape, dtype=np.uint64)
+    @abstractmethod
+    def step(self, new_img: np.ndarray) -> None:
+        raise NotImplementedError()
 
-def max_step(cumulative_img: np.ndarray, new_img: np.ndarray):
-    return np.maximum(cumulative_img, new_img)
+    @abstractmethod
+    def end(self) -> None:
+        raise NotImplementedError()
+    
+    def get_photo(self) -> Image.Image:
+        return Image.fromarray( np.uint8(self.res_photo) )
 
-def max_final(cumulative_img: np.ndarray, _: any):
-    return cumulative_img
+class AverageProcessing(BaseProcessing):
+    __slots__ = ('amount')
 
-def min_start(example_img: np.ndarray):
-    return 255 * np.ones(example_img.shape, dtype=np.uint64)
+    def __init__(self) -> None:
+        super().__init__()
+        self.amount = 0
 
-def min_step(cumulative_img: np.ndarray, new_img: np.ndarray):
-    return np.minimum(cumulative_img, new_img)
+    def start(self, example_img: np.ndarray) -> None:
+        self.res_photo = np.zeros(example_img.shape, dtype=np.uint64)
 
-def min_final(cumulative_img: np.ndarray, _: any):
-    return cumulative_img
+    def step(self, new_img: np.ndarray) -> None:
+        self.res_photo = self.res_photo + new_img.astype(dtype=np.uint64)
+        self.amount += 1
+        #print(self.amount)
+
+    def end(self) -> None:
+        self.res_photo = np.round( self.res_photo / self.amount ).astype(np.uint64)
+
+class MaximumProcessing(BaseProcessing):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def start(self, example_img: np.ndarray):
+        self.res_photo = np.zeros(example_img.shape, dtype=np.uint64)
+
+    def step(self, new_img: np.ndarray):
+        self.res_photo = np.maximum(self.res_photo, new_img)
+
+    def end(self):
+        pass
+
+class MinimumProcessing(BaseProcessing):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def start(self, example_img: np.ndarray):
+        self.res_photo = 255 * np.ones(example_img.shape, dtype=np.uint64)
+
+    def step(self, new_img: np.ndarray):
+        self.res_photo = np.minimum(self.res_photo, new_img)
+
+    def end(self):
+        pass
+
+FUNC_TO_CLASS: dict[str, BaseProcessing] = {
+    'avg': AverageProcessing(),
+    'max': MaximumProcessing(),
+    'min': MinimumProcessing()
+}
+
+# def avg_start(example_img: np.ndarray):
+#     return np.zeros(example_img.shape, dtype=np.uint64)
+
+# def avg_step(cumulative_img: np.ndarray, new_img: np.ndarray):
+#     return cumulative_img + new_img
+
+# def avg_final(cumulative_img: np.ndarray, amount: int):
+#     return np.round( cumulative_img / amount )
+
+# def max_start(example_img: np.ndarray):
+#     return np.zeros(example_img.shape, dtype=np.uint64)
+
+# def max_step(cumulative_img: np.ndarray, new_img: np.ndarray):
+#     return np.maximum(cumulative_img, new_img)
+
+# def max_final(cumulative_img: np.ndarray, _: any):
+#     return cumulative_img
+
+# def min_start(example_img: np.ndarray):
+#     return 255 * np.ones(example_img.shape, dtype=np.uint64)
+
+# def min_step(cumulative_img: np.ndarray, new_img: np.ndarray):
+#     return np.minimum(cumulative_img, new_img)
+
+# def min_final(cumulative_img: np.ndarray, _: any):
+#     return cumulative_img
 
 def main():
     args = get_args()
 
+    processing: BaseProcessing = FUNC_TO_CLASS[args.func]
+
     clips_amount = None
     with VideoFileClip(args.file) as video_file:
+        if video_file.rotation in (90, 270):
+            video_file = video_file.resize(video_file.size[::-1])
+            video_file.rotation = 0
+            
+        #print(video_file.size)
+        height, width = video_file.size
         tt = np.arange(0, video_file.duration, 1.0 / video_file.fps)
         
         image_clip: ImageClip = video_file.to_ImageClip(0)
+        
         img_arr = image_clip.get_frame(0)
-        result_img = eval(f'{args.func}_start')(img_arr)
-        clips_amount = len(tt)
+        #print(img_arr.shape)
+        needs_flip = list(img_arr.shape[:2]) != list(video_file.size)
+        print(f'Needs flip? {needs_flip}')
+        processing.start(img_arr)
 
-        dims = reversed(result_img.shape[:2])
-        print(f'Dimensions of result image: {dims[0]}x{dims[1]}')
-        step_func = eval(f'{args.func}_step')
         for i, t in tqdm(list(enumerate(tt)), ascii=True, dynamic_ncols=True):
             image_clip: ImageClip = video_file.to_ImageClip(t)
-            img_arr = image_clip.get_frame(0)
-            result_img = step_func(result_img, img_arr)
+            img_arr: np.ndarray = image_clip.get_frame(0)
+            processing.step(img_arr)
     
-    result_photo = eval(f'{args.func}_final')(result_img, clips_amount)
+    processing.end()
 
-    # width, height, color_depth = result_photo.shape
-    result_img = Image.fromarray( np.uint8(result_photo) )
-    result_img.save(f'{ os.path.splitext(args.file)[0] }-{args.func}.png')
+    result_image: Image.Image = processing.get_photo()
+    result_image.resize((width, height))
+    result_image.save(f'{ os.path.splitext(args.file)[0] }-{args.func}.png')
 
 if __name__ == '__main__':
     main()
---
+++ static/js/sse-process.js
@@ -0,0 +1,24 @@
+const appContainer = document.querySelector('div.app')
+const outside = document.querySelector('div.outside')
+const bar = document.querySelector('div.inside')
+
+const sse = new EventSource('/api/processing', { withCredentials: true})
+
+sse.onmessage = e => {
+    // progress message
+    console.log(`${e.data}`)
+    const parts = e.data.split('/')
+    const current = parseInt(parts[0])
+    const total = parseInt(parts[1])
+
+    // update UI
+    const baseWidth = 300 //parseInt(outside.style.width)
+    const barWidth = Math.floor( (current / total) * baseWidth )
+    bar.style.width = `${barWidth}px`
+
+    // redirect if progress ended
+    if (parts[0] == parts[1]){
+        sse.close()
+        window.location.pathname = '/result_file'
+    }        
+}
